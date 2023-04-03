372fe5f59a5c040d6051c71848bd9e52022c5ed5
Yerkebulan <128957554+yerkebulan0@users.noreply.github.com>
2023-04-03 18:08:03 +0600

    nfac

---
+++ homework1.js
@@ -0,0 +1,75 @@
+//>>>TUSK 1 <<<<
+// alert('Very welcome!');
+// let userF = confirm('Тебе есть 18?');
+// let ans;
+// if (userF = true){
+//     ans = "yes";
+// }
+
+//>>>>>>>TUSK 2<<<<<<
+// let user = prompt('are you men or women?');
+// userAns = user;
+
+
+// let userName = prompt('What`s your name?');
+// userNameis = userName;
+// console.log(ans);
+// console.log("User`s name is:"+userNameis);
+// console.log("user`s gender is:"+ userAns);
+
+// let secondTusk = prompt('10')
+// if (secondTusk > 0){
+//     console.log(alert(secondTusk));
+// }
+
+// let a = +prompt('a?', '');
+// console.log(a) // string
+// a = +a
+// console.log(a) // number
+
+// switch (a){
+//     case 0:
+//         alert(0);
+//     break;
+//     case 1:
+//         alert(1);
+//     break;
+//     case 2 || 3:
+//         alert('2,3');
+//         break;
+
+// }
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+// let num = 100;
+// let total = 0;
+// for (let i = 0;i < num;i++){
+//     if(i % 2 == 0){
+//         total++;
+//     }
+// }console.log(total);
+// let num = 100;
+// let total = 0;
+// let j = 0;
+
+while (j < num){
+    if (j % 2 == 0){
+        total++;
+    }
+}console.log(total);
+
+
+
+// let i =0;
+// while (i < 3){
+//     i++;
+//     alert('number ${i}!');
+// }
